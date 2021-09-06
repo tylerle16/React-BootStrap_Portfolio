8a46b04051fc0fa646f37cae6e345c2f87397d6e
Tyler Le <Tylerle90@gmail.com>
2021-09-06 16:56:45 -0400

    Header Complete

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar';
 // imported react 
 import React from "react"
+import Header from './components/Header';
+import Particles from 'react-particles-js'
+
 function App() {
   return (
-<Navbar/>  );
+    <div>
+      <Particles
+      className="particles-canvas"
+      params={{particles:{number:{value:30,
+      density:{
+        enable:true,
+        value_area:900
+      }},
+        shape:{
+          type: "circle", stroke:{
+            width: 6,
+            color: "#f33501"
+          }
+        }}
+    
+    }}
+      />
+    <Navbar/>
+    <Header/>
+
+    </div> );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Typed from "react-typed"
+import Particles from 'react-particles-js'
+const Header = () => {
+    return (
+    <div className="Header-wraper">
+        <div className="main-info">
+
+            <h1>Web development and website promotions</h1>
+            <Typed
+            className="typed-text"
+            strings={["Web Design", "Web Development", "JavaScript, Python, Node.js, React, Express.js, PostgreSQL, Bootstrap, HTML, CSS, GitHub, JQuery, Redux."]}
+            typeSpeed={70}
+            backSpeed={50}
+            loop
+            
+            />
+            <a href="#" className="btn-main-offer">Contact Me</a>
+        </div>
+
+    </div>
+    )
+}
+
+export default Header
