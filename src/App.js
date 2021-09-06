338213bc7e819e4e3069270f5a1bccdd5289945a
Tyler Le <Tylerle90@gmail.com>
2021-09-06 14:53:23 -0400

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar';
+// imported react 
+import React from "react"
 function App() {
   return (
-<h2>hello</h2>
-  );
+<Navbar/>  );
 }
 
 export default App;
